eecf59b225c7e09ef93c692c3cffa815ab0dbacd
paulmboyce <paul.boyce@goobito.com>
2020-11-25 12:37:10 +0000

    refactor(app): change default search for small serverless deploy test

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import useYouTubeSearch from "../hooks/useYouTubeSearch";
 
 const App = () => {
 	const [featureVideo, setFeatureVideo] = useState(null);
-	const [term, setTerm] = useState("tai chi lisbon");
+	const [term, setTerm] = useState("surfing lisbon");
 	const [error, setError] = useState(null);
 	const [searchErr, videos, doSearch] = useYouTubeSearch(term);
 
